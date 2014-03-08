226342677e67ba18858ceead15f783499b405e58
Rezan Achmad <rezanachmad@gmail.com>
2014-03-08 15:49:21 +0700

    SQL Schema

---
+++ protected/data/calegkita.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 08, 2014 at 04:47 PM
+-- Server version: 5.5.35
+-- PHP Version: 5.5.9-1+sury.org~precise+1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `calegkita`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `caleg`
+--
+
+CREATE TABLE IF NOT EXISTS `caleg` (
+  `id` varchar(128) NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `is_read` tinyint(1) NOT NULL DEFAULT '0',
+  `unlike_count` int(10) unsigned NOT NULL DEFAULT '0',
+  `like_count` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dapil`
+--
+
+CREATE TABLE IF NOT EXISTS `dapil` (
+  `id` varchar(128) NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `lembaga` varchar(10) NOT NULL,
+  `count` int(10) unsigned NOT NULL,
+  `count_read` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(128) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `NIK` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
